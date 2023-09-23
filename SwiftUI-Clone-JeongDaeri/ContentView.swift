e86169ef05103f000f89da25281580b1caeae3d4
junseopark0331 <junseopark0331@icloud.com>
2023-09-23 15:39:33 +0900

    강좌 - 2

--- SwiftUI-Clone-JeongDaeri/ContentView.swift
+++ SwiftUI-Clone-JeongDaeri/ContentView.swift
@@ -1,14 +1,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // State 값의 변화를 감지 -> 뷰에 적용
+    @State
+    private var isActivated: Bool = false
+    
+    // 몸체
     var body: some View {
-        HStack {
-            MyVstackView()
-            MyVstackView()
-            MyVstackView()
-        }
-        .padding(10.0)
-        .background(Color.yellow)
+        
+        NavigationView {
+            VStack {
+                HStack {
+                    MyVstackView()
+                    MyVstackView()
+                    MyVstackView()
+                }
+                .padding(isActivated ? 50.0 : 10.0)
+                //
+                .background(isActivated ? Color.yellow : Color.black)
+                
+                // 탭 제스처 추가
+                .onTapGesture {
+                    print("HStack이 클릭되었다.")
+                    
+                    // 애니메이션과 함께
+                    withAnimation{
+                        // toggle() true 이면 false로 false 이면 true로
+                        self.isActivated.toggle()
+                    }
+                } // HStack
+                
+                NavigationLink(destination: MyTextView()) {
+                    Text("네비게이션")
+                        .font(.system(size: 40))
+                        .fontWeight(.bold)
+                        .padding()
+                        .background(Color.orange)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(30)
+                }
+                // 네비게이션 버튼(링크)
+            }
+            
+        } // NavigationView
     }
 }
 
